703225cd28b8ad9d5d1930925c12bf1a3e0ce1a4
随风 <2429734791@qq.com>
2019-05-21 21:07:57 +0800

    replace

--- src/main/java/IndexController.java
+++ src/main/java/IndexController.java
@@ -1,6 +1,3 @@
-package com.wangsl.git;
-
-
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
--- src/man/java/IndexController.java
+++
@@ -1,6 +0,0 @@
-public class IndexController {
-
-    public static void main(String[] args) {
-        System.out.println("gitpro->master->update10");
-    }
-}
