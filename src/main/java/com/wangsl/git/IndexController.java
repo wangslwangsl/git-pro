32cdcfc3788713414ecc68742b842867f28410ec
随风 <2429734791@qq.com>
2019-05-21 19:28:57 +0800

    replace webmvc module

---
+++ src/main/java/com/wangsl/git/IndexController.java
@@ -0,0 +1,16 @@
+package com.wangsl.git;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+
+    @ResponseBody
+    @RequestMapping("/welcome")
+    public String print() {
+        return "Hello World";
+    }
+}
