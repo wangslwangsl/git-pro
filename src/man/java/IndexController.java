648eec1fd28028afb0f972570b7bfea9ff3324f8
虚竹一梦 <2429734791@qq.com>
2019-05-21 12:56:51 +0800

    Update IndexController.java

--- src/man/java/IndexController.java
+++ src/man/java/IndexController.java
@@ -1,6 +1,6 @@
 public class IndexController {
 
     public static void main(String[] args) {
-        System.out.println("master->update3");
+        System.out.println("gitpro->master->update");
     }
 }
