ccecaea6974327c1ac066c535cb0d682eae8bfa4
随风 <2429734791@qq.com>
2019-05-22 11:24:02 +0800

    add

---
+++ src/main/java/com/git/MainClass.java
@@ -0,0 +1,9 @@
+package com.git;
+
+import org.springframework.boot.SpringApplication;
+
+public class MainClass {
+    public static void main(String[] args) {
+        SpringApplication.run(MainClass.class, args);
+    }
+}
